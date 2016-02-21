3ecdff81154b232e5cd5150fefa9c11634b36ef0
menghuanwd <651019063@qq.com>
2016-02-21 21:44:26 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
 Rails.application.routes.draw do
 
-  # constraints :subdomain => /^(admin(.*))$/i  do
-    # namespace :admin, path: '/' do
+  constraints :subdomain => /^(admin(.*))$/i  do
+    namespace :admin, path: '/' do
 
-    #   root 'editors_session#login'
+      root 'editors_session#login'
 
-    #   resources :editors_session do
-    #     collection do
-    #       get 'login'
-    #       get 'logout'
-    #     end
-    #   end
+      resources :editors_session do
+        collection do
+          get 'login'
+          get 'logout'
+        end
+      end
 
-    # end
-  # end
+    end
+  end
 
-  # constraints :subdomain => /^(test(.*))$/i  do
+  constraints :subdomain => /^(test(.*))$/i  do
     namespace :frontend, path: '/' do
 
       root 'main#index'
     end
-  # end
+  end
 
 end
