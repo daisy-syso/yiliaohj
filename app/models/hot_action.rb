e441650e16d3d5908c7b7b0fbf715e7324780fca
Teddy Jiang <agilejzl@gmail.com>
2016-02-23 17:41:00 +0800

    添加热门活动后台管理页面

--- app/models/hot_action.rb
+++ app/models/hot_action.rb
@@ -1,9 +1,13 @@
 # 热门活动
 class HotAction
   include Mongoid::Document
-  include Mongoid::Timestamps  
+  include Mongoid::Timestamps
+
+  mount_uploader :image_url, PictureUploader
 
   field :title, type: String
   field :image_url, type: String
   field :link, type: String
+
+  validates :title, uniqueness: true, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
       end
 
       resources :banners
+      resources :hot_actions
 
     end
   end
