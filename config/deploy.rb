5241ca6a382dd2ebcb1c0a849ba6e31a4c86eecb
menghuanwd <651019063@qq.com>
2016-02-24 18:45:47 +0800

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,7 +65,7 @@ task :deploy => :environment do
     invoke :'deploy:cleanup'
 
     to :launch do
-      # queue "bundle exec thin stop"
+      queue "bundle exec thin stop"
       queue "bundle exec thin start -p 6000 -e production -d"
       # queue "whenever -w"
     end
