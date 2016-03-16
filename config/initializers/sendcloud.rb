7ea39bdf2a7aef6a6b2a7feb0e18fa7fec1ec177
leif <leif@beansmile.com>
2016-03-16 23:12:12 +0800

    login page

--- config/initializers/sendcloud.rb
+++ config/initializers/sendcloud.rb
@@ -1,4 +1,4 @@
-Sendcloud.setup do |config|
-  config.api_user = 'yiliaohj_mail'
-  config.api_key = 'qMgh602Vx1zeL4kf'
-end
+# Sendcloud.setup do |config|
+#   config.api_user = 'yiliaohj_mail'
+#   config.api_key = 'qMgh602Vx1zeL4kf'
+# end
