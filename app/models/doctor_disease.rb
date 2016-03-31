51cab82cdf7acf907e45469a4845f17d5d7b35cc
zhaolongwen <zlw302323@gmail.com>
2016-03-31 20:14:51 +0800

    change vote method in doctor_disease

--- app/models/doctor_disease.rb
+++ app/models/doctor_disease.rb
@@ -2,25 +2,20 @@
 class DoctorDisease
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Redis::Objects
 
   belongs_to :doctor, index: true
   belongs_to :disease, index: true
 
-  field :votes, type: Integer, default: 0
-  field :voted_by, type: Array, default: -> {[]}
-
-  def voted_by?(user)
-    voted_by.include? user.try(:id).to_s
-  end
+  set :voters
+  delegate :count, to: :voters, prefix: true
 
   def change_like!(user)
     user_id = user.id.to_s
-    if voted_by.include? user_id
-      voted_by.delete user_id
+    if voters.member? user_id
+      voters.delete user_id
     else
-      voted_by << user_id
+      voters << user_id
     end
-    self.votes = voted_by.size
-    save
   end
 end
