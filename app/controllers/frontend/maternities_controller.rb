70fd53178c60dbcfaecf05867c24f7eb5baa3fa2
menghuanwd <651019063@qq.com>
2016-04-20 23:21:02 +0800

    症状，月子中心。

--- app/controllers/frontend/maternities_controller.rb
+++ app/controllers/frontend/maternities_controller.rb
@@ -2,7 +2,47 @@ module Frontend
   class MaternitiesController < FrontendController
     # 离我最近 最近发布 人气最高 评价最好
     def index
-      @maternities = Maternity.all.page(params[:page]).per(params[:per])
+      # @maternities = Maternity.all.page(params[:page]).per(params[:per])
+
+      @proviences = Provience.includes(:cities)
+
+      query = {}
+
+      # 分类
+      if params[:category_name]
+        query[:categories] = params[:category_name]
+      end
+
+      # 城市
+      city = params[:city] || $redis_position.get("#{request.remote_ip}_city") || '上海市'
+      query[:city] = City.where(name: city).first
+
+      @maternities = Maternity.includes(:comments).includes(:commodities).where(query)
+
+      if params[:sort_type].present?
+        case params[:sort_type]
+        when 'neighbour'
+          # 离我最近
+          latitude, longitude = $redis_position.get(request.remote_ip).split(',').map(&:to_f)
+          @maternities = @maternities.geo_near([longitude, latitude]).spherical.max_distance(1000).to_a
+        when 'new'
+          # 最近发布
+          @maternities = @maternities.desc(:created_at)
+        when 'star'
+          # 评价最好
+          @maternities = @maternities.desc(:star)
+        when 'click_count'
+          # 人气最高
+          @maternities = @maternities.desc(:click_count)
+        end
+      end
+
+      if params[:sort_type] == 'neighbour'
+        @maternities = Kaminari.paginate_array(@maternities).page(params[:page]).per(params[:per])
+      else
+        @maternities = @maternities.page(params[:page]).per(params[:per])
+      end
+
     end
 
     def show
--- app/controllers/frontend/symptoms_controller.rb
+++ app/controllers/frontend/symptoms_controller.rb
@@ -1,19 +1,20 @@
 module Frontend
   class SymptomsController < FrontendController
     def index
-      symptoms = Symptom.all#.page(params[:page]).per(10)
-      arr = {}
-      symptoms.each do |s|
-        if arr.keys.include? s.english
-          aa = arr[s.english]
-          aa << [s.id.to_s, s.name]
-          arr[s.english] = aa
-        else
-          arr[s.english] = [[s.id.to_s, s.name]]
+      if $redis_symptoms.hget('symptoms', 'A').blank?
+        symptoms = Symptom.all
+        hash = {}
+        symptoms.each do |s|
+          if hash.keys.include? s.english
+            aa = hash[s.english]
+            aa = aa + "|" + [s.id.to_s, s.name].join(',')
+            hash[s.english] = aa
+          else
+            hash[s.english] = [[s.id.to_s, s.name].join(',')].join('|')
+          end
+          $redis_symptoms.hset('symptoms', s.english, hash[s.english])
         end
       end
-
-      @symptoms = arr
     end
 
     def show
