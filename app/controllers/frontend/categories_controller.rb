9fe42ed12af2260b29c7cfe7056aa94e09ee89e0
menghuanwd <651019063@qq.com>
2016-05-07 13:14:37 +0800

    全部分类 医疗团购

--- app/controllers/frontend/categories_controller.rb
+++ app/controllers/frontend/categories_controller.rb
@@ -2,9 +2,41 @@ class Frontend::CategoriesController < FrontendController
   def index
     @hospital_categories = HospitalCategory.includes(:children).where(parent_id: nil)
 
-    # 000000000000000000000000   数据库bugs 暂时先这样写
     @disease_categories = Department.includes(:children).where(parent_id: nil)
 
     @doctor_categories = Department.includes(:children).where(parent_id: nil)
+
+    # ["男科", "整形", "体检", "中医", "妇幼", "牙科", "综合", "其它"]
+    @buffets =
+    [
+      {
+        name: '精准男科',
+        link: '/buffets?hospital_category_parent_name=男科'
+      },
+      {
+        name: '精准整形',
+        link: '/buffets?hospital_category_parent_name=整形'
+      },
+      {
+        name: '精准体检',
+        link: '/buffets?hospital_category_parent_name=体检'
+      },
+      {
+        name: '精准中医',
+        link: '/buffets?hospital_category_parent_name=中医'
+      },
+      {
+        name: '精准妇幼',
+        link: '/buffets?hospital_category_parent_name=妇幼'
+      },
+      {
+        name: '精准牙科',
+        link: '/buffets?hospital_category_parent_name=牙科'
+      },
+      {
+        name: '精准体检',
+        link: '/examinations'
+      }
+    ]
   end
 end
