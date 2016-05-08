cf1f56fe6b9167a61382a19d555557b63a182b38
dark <893191249@qq.com>
2016-05-08 10:24:59 +0800

    symptom load  ajax

--- app/controllers/frontend/symptoms_controller.rb
+++ app/controllers/frontend/symptoms_controller.rb
@@ -1,20 +1,23 @@
 module Frontend
   class SymptomsController < FrontendController
     def index
-      if $redis_symptoms.hget('symptoms', 'A').blank?
-        symptoms = Symptom.all
-        hash = {}
-        symptoms.each do |s|
-          if hash.keys.include? s.english
-            aa = hash[s.english]
-            aa = aa + '|' + [s.id.to_s, s.name].join(',')
-            hash[s.english] = aa
-          else
-            hash[s.english] = [[s.id.to_s, s.name].join(',')].join('|')
-          end
-          $redis_symptoms.hset('symptoms', s.english, hash[s.english])
-        end
+      @symptoms = Symptom.order(id: :asc)
+    end
+
+    def load
+      @symptom = Symptom.find(params[:id])
+      @symptoms = Symptom
+      .order(id: :asc)
+      .where(english: params[:english])
+      .where(:id.gt => @symptom.id).limit(10)
+
+      html = ''
+      @symptoms.each do |symptom|
+        html += %Q(<li class="symptom-item">
+          <a href="symptoms/#{symptom.id}" data-id="#{symptom.id}" data-english="#{params[:english]}">#{symptom.name}</a>
+      </li>)
       end
+      render text: html
     end
 
     def show
