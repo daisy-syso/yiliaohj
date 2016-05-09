6d4e7dcc4dc23ca7cb22f40316ec27350940144f
menghuanwd <651019063@qq.com>
2016-05-10 00:15:06 +0800

    filters

---
+++ app/controllers/frontend/all_categories_controller.rb
@@ -0,0 +1,22 @@
+class Frontend::AllCategoriesController < FrontendController
+
+  def index
+    words = params[:key]
+
+    case words
+    when 'hospitals'
+      @hospital_categories = HospitalCategory.where(parent_id: nil).includes(:children)
+
+      arr = []
+      @hospital_categories.each do |c|
+        arr << {
+          category_id: c.id.to_s,
+          category_name: c.name,
+          has_next: true
+        }
+      end
+
+      render json: arr.to_json
+    end
+  end
+end
