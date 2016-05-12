e3a56c9f8bfaf78f42400165a33397e0bbeb1e02
menghuanwd <651019063@qq.com>
2016-05-12 17:37:38 +0800

    update

--- scripts/disease_doctor_ids.rb
+++ scripts/disease_doctor_ids.rb
@@ -1,9 +1,7 @@
 Disease.all.each do |h|
-  h.each do |h|
-    doctor_ids = h.doctor_ids
-    if doctor_ids.class.name == 'String'
-      h.doctor_ids = doctor_ids.split(',')
-      puts h.save
-    end
+  doctor_ids = h.doctor_ids
+  if doctor_ids.class.name == 'String'
+    h.doctor_ids = doctor_ids.split(',')
+    puts h.save
   end
 end
