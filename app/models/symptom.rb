cb19e94e0bfb195d31353354350b08633c3bba77
ZengWeimao <so.zengtao@gmail.com>
2016-05-17 22:48:25 +0800

    修复一些中英文混杂的bug

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -13,19 +13,21 @@ class Symptom
   field :common_drug, type: String
   field :common_disease, type: String
   field :display_index, type: Integer, default: 0
-  field :index_array, type: Array, default: []
+  field :index_string, type: String, default: nil
 
   # callbacks
-  before_save :init_index_array #after_create is before before_save
-  def init_index_array
-    if self.index_array.empty?
-      self.index_array = PinYin.of_string(self.name)
-    end
+  before_save :init_index_string #after_create is before before_save
+  def init_index_string
+    # if self.index_string.blank?
+      self.index_string = PinYin.of_string(self.name).map do |item|
+        item[0].downcase
+      end.join('')
+    # end
   end
 
   # class methods to init history data Use like this: Symptom.init_index_array
   class << self
-    def init_index_array
+    def init_index_string
       Symptom.each do |s|
         s.save
       end
