29796dd895c4034a3a0e6057df8839c5f41d10aa
menghuanwd <651019063@qq.com>
2016-05-21 21:35:50 +0800

    symptoms

--- app/controllers/frontend/symptoms_controller.rb
+++ app/controllers/frontend/symptoms_controller.rb
@@ -1,23 +1,8 @@
 module Frontend
   class SymptomsController < FrontendController
     def index
-      @symptoms = Symptom.order(display_index: :asc)
-    end
+      @symptom_first_chars = Symptom.all.asc(:english).pluck(:english).collect {|c| c[0]}.uniq
 
-    def load
-      @symptom = Symptom.find(params[:id])
-      @symptoms = Symptom
-                  .order(display_index: :asc)
-                  .where(english: params[:english])
-                  .where(:display_index.gt => @symptom.display_index).limit(10)
-
-      html = ''
-      @symptoms.each do |symptom|
-        html += %(<li class="symptom-item">
-          <a href="symptoms/#{symptom.id}" data-id="#{symptom.id}" data-english="#{params[:english]}">#{symptom.name}</a>
-      </li>)
-      end
-      render text: html
     end
 
     def show
