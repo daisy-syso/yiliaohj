cbb181283a49fd33e517abf9938a19611ad560a6
menghuanwd <651019063@qq.com>
2016-05-21 09:15:53 +0800

    fixubg

--- app/controllers/frontend/strategy_categories_controller.rb
+++ app/controllers/frontend/strategy_categories_controller.rb
@@ -6,7 +6,8 @@ module Frontend
 
     def sub_strategy_categories
       # 573eafd55b2cd71e1c185084
-      @sub_strategy_categories = StrategyCategory.where(strategy_category_id: params[:id])
+      # debugger
+      @sub_strategy_categories = StrategyCategory.where(parent_id: params[:id])
     end
   end
 end
