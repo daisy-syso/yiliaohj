c873da87d16d0bda00c756695ef2001c6e58dd4f
menghuanwd <651019063@qq.com>
2016-05-21 09:10:26 +0800

    add strategies

--- app/models/strategy_category.rb
+++ app/models/strategy_category.rb
@@ -6,7 +6,10 @@ class StrategyCategory
   belongs_to :parent, class_name: 'StrategyCategory', inverse_of: :children, index: true
   has_many :children, class_name: 'StrategyCategory', inverse_of: :parent
 
+  mount_uploader :image_url, PictureUploader
+
   field :name, type: String
+  field :image_url, type: String, default: ''
 
   has_many :strategies
 end
