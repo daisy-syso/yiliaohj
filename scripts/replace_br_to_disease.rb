1e84567d408b1f30f4bc386facfc2e917b104dd4
menghuanwd <651019063@qq.com>
2016-05-25 00:45:22 +0800

    更新疾病基本信息 格式化 脚本

--- scripts/replace_br_to_disease.rb
+++ scripts/replace_br_to_disease.rb
@@ -1,14 +1,23 @@
-Disease.all.each do |disease|
-	p disease.id
-  p replace_to_br(disease)
+def replace_to_br(disease)
+  disease.description.gsub!("\r\n\t", "<br/>")
+  disease.etiology.gsub!("\r\n\t", "<br/>")
+  disease.treatent.gsub!("\r\n\t", "<br/>")
+  disease.prevention.gsub!("\r\n\t", "<br/>")
+  disease.diet.gsub!("\r\n\t", "<br/>")
+  disease.symptom.gsub!("\r\n\t", "<br/>")
+  disease.examination.gsub!("\r\n\t", "<br/>")
+  disease.save
 end
 
-def replace_to_br(disease)
-  disease.description = disease.description.gsub("\r\n\t", "<br/>")
-  disease.etiology 	  = disease.etiology.gsub("\r\n\t", "<br/>")
-  disease.treatent    = disease.treatent.gsub("\r\n\t", "<br/>")
-  disease.prevention  = disease.prevention.gsub("\r\n\t", "<br/>")
-  disease.diet        = disease.diet.gsub("\r\n\t", "<br/>")
-  disease.symptom     = disease.symptom.gsub("\r\n\t", "<br/>")
+def clear_description(disease)
+  disease.description.gsub!("　　", '<br />')
   disease.save
+end
+
+Disease.all.each_slice(100) do |diseases|
+  diseases.each do |d|
+    puts "#{d.id.to_s}"
+    puts replace_to_br(d)
+    puts clear_description(d)
+  end
 end
