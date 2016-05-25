a580219523e7d023cf0aa1de37ed027a51a492e5
menghuanwd <651019063@qq.com>
2016-05-25 08:14:44 +0800

    fuck

--- scripts/replace_br_to_disease.rb
+++ scripts/replace_br_to_disease.rb
@@ -17,18 +17,18 @@
 Disease.all.each_slice(100) do |diseases|
   diseases.each do |d|
     puts "#{d.id.to_s}"
-    puts replace_to_br(d)
-    puts clear_description(d)
+    # puts replace_to_br(d)
+    # puts clear_description(d)
 
-    disease.description.gsub!("\r\n\t", "<br/>")
-    disease.etiology.gsub!("\r\n\t", "<br/>")
-    disease.treatent.gsub!("\r\n\t", "<br/>")
-    disease.prevention.gsub!("\r\n\t", "<br/>")
-    disease.diet.gsub!("\r\n\t", "<br/>")
-    disease.symptom.gsub!("\r\n\t", "<br/>")
-    disease.examination.gsub!("\r\n\t", "<br/>")
+    d.description.gsub!("\r\n\t", "<br/>")
+    d.etiology.gsub!("\r\n\t", "<br/>")
+    d.treatent.gsub!("\r\n\t", "<br/>")
+    d.prevention.gsub!("\r\n\t", "<br/>")
+    d.diet.gsub!("\r\n\t", "<br/>")
+    d.symptom.gsub!("\r\n\t", "<br/>")
+    d.examination.gsub!("\r\n\t", "<br/>")
 
-    disease.description.gsub!("　　", '<br />')
-    disease.save
+    d.description.gsub!("　　", '<br />')
+    d.save
   end
 end
