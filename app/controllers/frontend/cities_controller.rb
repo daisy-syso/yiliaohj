4c6a178e9f74b296f1ec01e82345cf31e60b36eb
menghuanwd <651019063@qq.com>
2016-05-26 18:11:53 +0800

    定位

--- app/controllers/frontend/cities_controller.rb
+++ app/controllers/frontend/cities_controller.rb
@@ -2,6 +2,21 @@ class Frontend::CitiesController < FrontendController
 
   def index
     @hot_cities = City.where(is_hot: true)
+
+    city_chars = City.all.asc(:english).pluck(:english).collect {|c| c}.uniq
+
+    @all_cities = {}
+    city_chars.each do |char|
+      arr = []
+      City.where(english: char).asc(:english).each do |s|
+        # puts s.english
+        # $redis_symptoms.rpush char, "#{s.id.to_s},#{s.name}"
+        arr << s.name
+      end
+      @all_cities[:"#{char}"] = arr
+    end
+
+    @current_city = '上海市'
   end
 
   def cities_search
