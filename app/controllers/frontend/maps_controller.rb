e21ed0d5796ab25f1f376b81933eb8cf00b4f733
AlanViast <AlanViast@gmail.com>
2016-05-26 17:11:21 +0800

    maps 不能正常使用的问题

--- app/controllers/frontend/maps_controller.rb
+++ app/controllers/frontend/maps_controller.rb
@@ -1,5 +1,6 @@
 module Frontend
   class MapsController < FrontendController
+    layout false
     def index
       @lng = params[:location].split(',')[1]
       @lat = params[:location].split(',')[0]
