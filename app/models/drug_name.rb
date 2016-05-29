fc48960b21b71a37eb15ef62875593f14f68a990
menghuanwd <651019063@qq.com>
2016-05-29 21:34:22 +0800

    update

--- app/models/drug_name.rb
+++ app/models/drug_name.rb
@@ -13,11 +13,13 @@ class DrugName
   field :code, type: String
   field :spec, type: String
   # 价格范围 [25, 60]
-  field :price_range, type: Array, default: -> {[]}
+  field :price_high, type: Integer
+  field :price_low, type: Integer
+  # field :price_range, type: Array, default: -> {[]}
   # 药品分类
   field :drug_category, type: String
   # 疾病名称
   field :disease_names, type: Array, default: -> {[]}
-  # 推广的药店ID
+  # 推广的药品ID
   field :recommand_drug_id, type: String
 end
