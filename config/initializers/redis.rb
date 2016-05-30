0b0bf1e7a8790ee017d23807fd5664c469d6dd15
menghuanwd <651019063@qq.com>
2016-05-30 16:53:50 +0800

    two redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 redis_basic = {
-  host: Settings.redis.host,
-  port: Settings.redis.port
+  host: Settings.cloud_redis.host,
+  port: Settings.cloud_redis.port
 }
 
 require 'connection_pool'
-Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) { Redis.new redis_basic.merge(db: Settings.redis.position) }
+Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) { Redis.new redis_basic.merge(db: 0) }
 
-$redis_position = Redis.new redis_basic.merge(db: Settings.redis.position)
+$redis_position = Redis.new redis_basic.merge(db: 0)
 
-$redis_sms = Redis.new redis_basic.merge(db: Settings.redis.sms)
+$redis_sms = Redis.new redis_basic.merge(db: 0)
 
-$redis_symptoms = Redis.new redis_basic.merge(db: Settings.redis.symptoms)
+$redis_symptoms = Redis.new redis_basic.merge(db: 0)
